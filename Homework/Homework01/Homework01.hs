cd04b02f11fb9b72cfbf3ea8b5cc3ddc583ccf2d
Thomas Leahy <89755088+Surdano@users.noreply.github.com>
2024-02-21 09:51:35 -0800

    Completed Homework01.hs

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -2,15 +2,26 @@
 -- Question 1
 -- Write a multiline comment below.
 
+{- This is what
+a multiline comment
+looks like -}
+
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+
+multiplyThree x = x*3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
 
+circleArea r = pi * r^2
+
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+
+volumeOfCylinder r h = pi * r^2 * h
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
 
+volumeCheck r h = pi * r^2 * h >= 42
